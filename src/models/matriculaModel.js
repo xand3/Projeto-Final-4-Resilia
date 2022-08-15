f5bd4e00d2e0299e830df39a28afae3726e1fb1a
matheushage <hagearaujo@gmail.com>
2022-08-15 20:59:32 -0300

    fix: refactoring the code with my teammates

---
+++ src/models/matriculaModel.js
@@ -0,0 +1,9 @@
+class Matricula {
+  constructor(id_aluno, id_turma, status) {
+    this.id_aluno = id_aluno;
+    this.id_turma = id_turma;
+    this.status = status;
+  }
+}
+
+export default Matricula;
