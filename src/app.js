327617531708ed5447c94a5a24ad2b32fb88a3b8
Camila <camilanachozerra@gmail.com>
2022-08-16 17:27:01 -0400

    resolvendo conflito de pull da branch main no app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
 import express from "express";
-<<<<<<< HEAD
 import routerCoordenacao from "./routes/coordenacaoRouter.js";
-
-const app = express();
-app.use(express.json());
-
-app.use("/", routerCoordenacao);
-=======
-
-import { rmSync } from "fs";
-
 import routerAlunos from "./routes/alunoRoutes.js";
 import routerTurma from "./routes/turmaRoutes.js";
-
 import routerDocente from "./routes/docenteRoutes.js"
+import { rmSync } from "fs";
+
+
 
 const app = express()
 app.use(express.json())
-
+app.use("/", routerCoordenacao);
 app.use('/', routerDocente)
-
 app.use('/',routerAlunos);
 app.use('/',routerTurma);
-
 app.use("/aluno", routerAlunos)
 app.use("/turma",routerTurma)
->>>>>>> 9e04a267657873c5b1a16a9e90cb3a4a630daedf
 
 app.listen(3000, () => {
   console.log("api rodando");
